78f27a56da33a010d332693a300025c78c9c111b
usuario <usuario@pc21-099>
2023-10-03 16:54:16 +0200

    Este es el commit de la Sesion1

--- src/Sesion/Calculadora.java
+++ src/Sesion/Calculadora.java
@@ -1,5 +1,24 @@
 package Sesion;
 
 public class Calculadora {
-
+	
+	public static int suma ( int a, int b)
+	{
+		return (a+b);
+	}
+	public static int resta(int a, int b)
+	{
+		return (a-b);
+	}
+	public static int multiplica(int a, int b)
+	{
+		return (a*b);
+	}
+	public static int divide(int a, int b)
+	{
+		if(b==0)
+			return -1;
+		else
+			return a/b;
+	}
 }
